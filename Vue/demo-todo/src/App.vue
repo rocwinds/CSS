b96ded2898d2823e70d4df7427e766d27ad29999
yesheng <yeshengwk@outlook.com>
2020-06-29 12:08:30 +0800

    webpack几种常用loader介绍

--- Vue/demo-todo/src/App.vue
+++ Vue/demo-todo/src/App.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>this is VueApp</div>
+    <div>
+        <h1>this is VueApp</h1>
+    </div>
 </template>
 
 <script>
+// import img from './assets/images/bg.jpg'
+// import './assets/images/2.svg'
+// import './assets/styles/test1.css'
+import './assets/styles/global.styl'
+
+
 export default {
-    name:'App'//默认输出
+    name:'App'//默认输出名
+    /*
+    created(){
+        console.log(img)
+    }
+    //vue里初始化完成就会执行这里的语句，可以打印一下img的信息，webpack打包之后会返回文件名，就会打印文件名了
+    */
 }
 </script>
 
-<style scope>
-
+<style lang='stylus' scoped>//lang='stylus'代表了App.vue的style样式使用的是stylus语法，scoped代表了这些样式只会在本文件生效，style标签里的语法就按照stylus写就行了（需要注意下因为vscode插件省略了{}和; ）
+    h1
+        color:red
 </style>
--- Vue/demo-todo/webpack.config.js
+++ Vue/demo-todo/webpack.config.js
@@ -25,7 +25,39 @@ module.exports = {
             test:/\.vue$/,
             loader:'vue-loader'
             //test代表了正则表达式，这里匹配的是以vue结尾的文件，而loader代表了用什么loader完成打包
-        }]
+        },
+        {
+            test: /\.(jpg|jpeg|png)$/,
+            loader: 'file-loader',
+            options: {
+               name: '[name].[ext]'//文件名占位符，表示使用'[原文件名].[原文件扩展名]'输出文件
+            }
+            
+        },
+        // {
+        //     test: /\.(jpg|jpeg|png|svg)$/,
+        //     loader: 'url-loader',
+        //     options: {
+        //        name: '[name].[ext]',//文件名占位符，表示使用'[原文件名].[原文件扩展名]'输出文件
+        //        limit: 4096//单位是byte，当jpg、jpeg、png、svg文件小于4096byte时, 会以base64形式直接打包到js文件中
+        //     }
+        // },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            //正则匹配结尾为css的文件，使用css-loader再用style-loader，值得一提之前都是写loader,这里用use是因为用了多个loader所以写法不一样，记住use是按照从右到左, 从下到上的顺序依次执行的，不能随意乱写
+        },
+        // {
+        //     test: /\.styl(us)?$/,
+        //     use: ['style-loader', 'css-loader', 'stylus-loader']
+        //     //正则匹配以styl结尾或者stylus结尾的文件，依次根据配置从右往左调用'stylus-loader'、'css-loader'、'style-loader'，就是stylus-loader先会将stylus文件转换成css文件，然后css-loader将全部css汇总生成一个总的css文件，然后style-loader将总的css文件代码挂载到vue页面的<style>标签对里
+        // },
+        {
+            test: /\.styl(us)?$/,
+            use: ['vue-style-loader', 'css-loader', 'stylus-loader']
+            //用法类似style-loader，只不过专门用于打包vue的stylus样式，另外不能两个loader一起生效，会报错
+        }
+        ]
     },
 
     //配置vue-loader插件，需要实例化VueLoaderPlugin,因为plugins可能会有很多插件对象所以也是一个数组
